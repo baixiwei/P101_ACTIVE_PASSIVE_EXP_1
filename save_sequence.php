daae93f20d4f57e6923303beda08439c75a871aa
Josh de Leeuw <josh.deleeuw@gmail.com>
2014-01-07 14:56:38 -0500

    adding in mysql escapes for security

--- save_sequence.php
+++ save_sequence.php
@@ -6,7 +6,7 @@ include('p101_database_connect.php');
 $sequenceobj = $_POST['sequenceobj']; // sequence object data
 $subjid = $_POST['subjid']; // subject
 
-$query = 'INSERT INTO sequences (subjid, sequenceobj) VALUES ('.$subjid.',\''.$sequenceobj.'\')';
+$query = 'INSERT INTO sequences (subjid, sequenceobj) VALUES ('.mysql_real_escape_string($subjid).',\''.mysql_real_escape_string($sequenceobj).'\')';
 
 $result = mysql_query($query);
 
