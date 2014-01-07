eb83ca43f0a0d71e7a33beac0e5dc121101ffc92
Josh de Leeuw <josh.deleeuw@gmail.com>
2014-01-07 14:04:43 -0500

    fixing string notation for value in save

--- save_sequence.php
+++ save_sequence.php
@@ -6,7 +6,7 @@ include('p101_database_connect.php');
 $sequenceobj = $_POST['sequenceobj']; // sequence object data
 $subjid = $_POST['subjid']; // subject
 
-$query = 'INSERT INTO sequences (subjid, sequenceobj) VALUES ('.$subjid.','.$sequenceobj.')';
+$query = 'INSERT INTO sequences (subjid, sequenceobj) VALUES ('.$subjid.',\''.$sequenceobj.'\')';
 
 echo $query;
 
