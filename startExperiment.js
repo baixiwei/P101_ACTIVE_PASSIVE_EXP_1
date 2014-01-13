7a79aef8b9ae5fb83a92f1baab66c97ae740c4d4
baixiwei <baixiwei@gmail.com>
2014-01-12 21:04:10 -0500

    added array prototype methods

--- startExperiment.js
+++ startExperiment.js
@@ -3,6 +3,51 @@
 // startExperiment, doPretest, doInstructions, doTraining, endExperiment
 ///////////////////////////////////////////////////////////////////////////
 
+// define some prototype methods in case they're not present in the browser
+if (!Array.prototype.filter)
+{
+  Array.prototype.filter = function(fun /*, thisp*/)
+  {
+    var len = this.length;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    var res = new Array();
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in this)
+      {
+        var val = this[i]; // in case fun mutates this
+        if (fun.call(thisp, val, i, this))
+          res.push(val);
+      }
+    }
+
+    return res;
+  };
+}
+
+if (!Array.prototype.map)
+{
+  Array.prototype.map = function(fun /*, thisp*/)
+  {
+    var len = this.length;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    var res = new Array(len);
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in this)
+        res[i] = fun.call(thisp, this[i], i, this);
+    }
+
+    return res;
+  };
+}
+
 // global variables
 //  global variables assigned by startExperiment call
 // should only be used within startExperiment.js
